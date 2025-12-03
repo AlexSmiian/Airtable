5ea70179ce5f5d65a53e6b1f33c8d15bff9a6ad3
oleksandr_smiian <oleksandr_smiian@tchdm.live>
2025-12-03 22:13:12 +0200

    init project

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from backend!" });
+});
+
+app.get('/api/privet', (req, res) => {
+    res.json({ message: 'Hello from backend Privet' });
+});
+
+app.get('/api/www', (req, res) => {
+    res.json({ message: 'Hello from backend WWW' });
+});
+
+app.listen(5000, () => console.log("Backend running on port 5000"));
