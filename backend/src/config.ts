492a8afd6e7cc4fb96eff7345cc4cf775835975a
oleksandr_smiian <oleksandr_smiian@tchdm.live>
2025-12-09 14:37:19 +0200

    change backend port 5000 ---> 7000

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local';
 dotenv.config({path: path.resolve(__dirname, '../../', envFile)});
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 7000;
 export const PG_USER = process.env.POSTGRES_USER || 'postgres';
 export const PG_PASSWORD = process.env.POSTGRES_PASSWORD || 'postgres';
 export const PG_DB = process.env.POSTGRES_DB || 'airtable';
